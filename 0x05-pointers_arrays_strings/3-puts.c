40fd7f25d9a0ee86001c68d9a2ba8947ce3e1465
Bekono-Ottou <bekonoottou@gmail.com>
2022-07-12 17:08:39 -0700

    3. I do not fear computers. I fear the lack of them -5

--- 0x05-pointers_arrays_strings/3-puts.c
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -7,16 +7,10 @@
  */
 void _puts(char *str)
 {
-	int count = 0;
+  for (; *str != '\0'; str++)
 
-	while (count >= 0)
-	{
-		if (str[count] == '\0')
-		{
-			_putchar('\n');
-			break;
-		}
-		_putchar(str[count]);
-		count++;
-	}
+  {
+    _putchar(*str);
+  }
+  _putchar('\n');
 }
